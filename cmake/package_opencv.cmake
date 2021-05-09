cc2e457b12bfdfa21321eb8a7c4c185becd70e67
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-05-09 20:48:30 +0800

    add github actions for build and ctest

---
+++ cmake/package_opencv.cmake
@@ -0,0 +1,15 @@
+include(${CMAKE_SOURCE_DIR}/cmake/OpenCVPath.cmake)
+IF (WIN32)
+    set(OpenCV_BINS "${OpenCV_DIR}/x64/vc16/bin")
+    # work for windows10-2004,vs2019
+ELSEIF (APPLE)
+    message(FATAL_ERROR "THIS CMAKE SCRIPT HAVEND TEST IN APPLE")
+ELSEIF (UNIX)
+    message(FATAL_ERROR "This cmake script have not test opencv in unix-like system")
+ENDIF ()
+
+Output_variable(OpenCV_FOUND)
+Output_Paths()
+find_package(OpenCV REQUIRED)
+Output_Paths()
+Output_variable(OpenCV_FOUND)
--- cmake/windows_OpenCV.cmake
+++
@@ -1,8 +0,0 @@
-include(${CMAKE_SOURCE_DIR}/cmake/OpenCVPath.cmake)
-set(OpenCV_BINS "${OpenCV_DIR}/x64/vc16/bin") # work for windows10-2004
-
-Output_variable(OpenCV_FOUND)
-Output_Paths()
-find_package(OpenCV REQUIRED)
-Output_Paths()
-Output_variable(OpenCV_FOUND)
