1ebd38d8dfda1b4c87febded8454ecb2f2e0bdd6
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-06-30 21:19:36 +0800

    improve use of cpp20.
    </subject>
    
    Branch: master
    
    <type>:
    - [x] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>

--- src/template_helper.hpp
+++ src/template_helper.hpp
@@ -21,89 +21,91 @@
  * @Date: 2020-05-24 17:56:20
  * @LastEditors  : nanoseeds
  */
-#ifndef CS205_C_CPP_CS205_PROJECT_2020S_SRC_TEMPLATE_HELPER_H
-#define CS205_C_CPP_CS205_PROJECT_2020S_SRC_TEMPLATE_HELPER_H
+#ifndef CS205_C_CPP_CS205_PROJECT_2020S_SRC_TEMPLATE_HELPER_HPP
+#define CS205_C_CPP_CS205_PROJECT_2020S_SRC_TEMPLATE_HELPER_HPP
 
 #include <type_traits>
+#include <concepts>
 
-// #define Multiply_Result_t_Macro decltype(std::declval<T1>() * std::declval<T2>())
+using std::type_identity;
+using std::is_same;
+using std::is_same_v;
 
-template<typename T1, typename T2>
-constexpr bool is_same() { return std::is_same<T1, T2>::value; }
+template<bool B, typename T, typename F>
+struct conditional : type_identity<T> {};
+
+template<typename T, typename F>
+struct conditional<false, T, F> : type_identity<F> {};
+
+template<bool B, typename T, typename F>
+using conditional_t = typename conditional<B, T, F>::type;
 
 template<typename T>
 concept is_complex = requires(T f) {
-    f.real();
-    f.imag();
-    std::conj(f);
-    f /= 1.0f;
-    std::constructible_from<T, typename T::value_type, typename T::value_type>;
-    std::same_as<std::complex<typename T::value_type>, T>;
+	f.real();
+	f.imag();
+	std::conj(f);
+	f /= 1.0f;
+	std::constructible_from<T, typename T::value_type, typename T::value_type>;
+	std::same_as<std::complex<typename T::value_type>, T>;
 };
 template<typename T>
-struct complex_inside_type : std::false_type {
-    //static_assert(!is_complex<T>(), "complex_inside_type: std::false_type");
-    using Type = T;
-};
+struct complex_inside : type_identity<T> {};
 template<typename T>
-struct complex_inside_type<std::complex<T>> : std::true_type {
-    //static_assert(is_complex<T>(), "complex_inside_type<std::complex<T>>: std::true_type");
-    using Type = T;
-};
+struct complex_inside<std::complex<T>> : type_identity<T> {};
 
 template<typename T>
-using complex_inside_type_t = typename complex_inside_type<T>::Type;
+using complex_inside_t = typename complex_inside<T>::type;
 
 template<typename T1, typename T2>
-struct Minus_Result {
-    using Type = decltype(std::declval<T1>() - std::declval<T2>());
+struct Minus_Result : type_identity<decltype(std::declval<T1>() - std::declval<T2>())> {
 };
 
 template<typename T1, typename T2>
-using Minus_Result_t = typename Minus_Result<T1, T2>::Type;
+using Minus_Result_t = typename Minus_Result<T1, T2>::type;
 
 
 template<typename T1, typename T2>
-struct Add_Result {
-    using Type = decltype(std::declval<T1>() + std::declval<T2>());
+struct Add_Result : type_identity<decltype(std::declval<T1>() + std::declval<T2>())> {
 };
 
 template<typename T1, typename T2>
-using Add_Result_t = typename Add_Result<T1, T2>::Type;
+using Add_Result_t = typename Add_Result<T1, T2>::type;
 
 
 template<typename T1, typename T2>
-struct Multiply_Result {
-    using Type = decltype(std::declval<T1>() * std::declval<T2>());
+struct Multiply_Result : type_identity<decltype(std::declval<T1>()*std::declval<T2>())> {
 };
 
 template<typename T1, typename T2>
-using Multiply_Result_t = typename Multiply_Result<T1, T2>::Type;
-
-template<typename T1, typename T2>
-struct Divide_Result {
-    using Type = decltype(std::declval<T1>() / std::declval<T2>());
-};
-template<typename T1, typename T2>
-using Divide_Result_t = typename Divide_Result<T1, T2>::Type;
+using Multiply_Result_t = typename Multiply_Result<T1, T2>::type;
 
 template<typename T1>
-using divide_t = decltype(std::declval<T1>() / std::declval<double>());
+using divide_t = decltype(std::declval<T1>()/std::declval<double>());
 
 
 template<typename T>
-concept has_conj = requires(T f) {
-    f.conj();
-};
+concept has_conj = requires(T f) { f.conj(); };
 
 template<typename T>
 T from_char_array(unsigned char const *buffer) {
-    T will_return;
-    auto *dp = reinterpret_cast<unsigned char *>(&will_return);
-    std::copy(buffer, buffer + sizeof(T), dp);
-    return will_return;
+	T will_return;
+	auto *dp = reinterpret_cast<unsigned char *>(&will_return);
+	std::copy(buffer, buffer + sizeof(T), dp);
+	return will_return;
 }
 
-#define MY_IF0(...) typename std::enable_if<(bool)(__VA_ARGS__), int >::type
-#define MY_IF(...) MY_IF0(__VA_ARGS__) = 0
-#endif //CS205_C_CPP_CS205_PROJECT_2020S_SRC_TEMPLATE_HELPER_H
+template<typename T, typename U, typename... Rest>
+struct is_one_of : conditional_t<
+	is_same_v<T, U>, std::true_type, is_one_of<T, Rest...>> {
+};
+
+template<typename T, typename U>
+struct is_one_of<T, U> : conditional_t<
+	is_same_v<T, U>, std::true_type, std::false_type> {
+};
+template<typename T>
+concept opencv_type = requires(T f) {
+	is_one_of<T, uint8_t, int8_t, uint16_t, int16_t, int32_t, float, double>::value;
+};
+#endif //CS205_C_CPP_CS205_PROJECT_2020S_SRC_TEMPLATE_HELPER_HPP
